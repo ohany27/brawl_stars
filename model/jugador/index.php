65cfcd9569b2c35655f2828c2107974d512bca61
Yareth-27 <yarethohany.6704@gmail.com>
2024-04-16 19:24:12 -0500

    Vista Jugador

---
+++ model/jugador/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Varela+Round&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/jugador.css">
+    <title>Index Jugadores</title>
+    
+</head>
+<body>
+
+
+   <div id="contenedor">
+    <header>
+    <div class="container-fluid mt-2">
+        <div class="play">
+            <div class="player">
+                <img src="../../img/icono_jugador.webp" class="img">
+                <p>Nombre</p>
+            </div>
+            <div class="info" >
+                <p>Nivel:</p>
+                <p>Puntos:</p>
+            </div>
+        </div>
+    </div>
+    </header>
+    <section id="contenido">
+    <div class="content-wrapper">
+        <img src="../../img/primo.png" alt="Descripción de la imagen">
+        <button type="button" class="btn btn-primary diagonal"><a href="#">UNIRSE A PARTIDA</a></button>
+        
+    </div>
+</section>
+    <aside>
+    <div class="opciones">
+            <div class="aside">
+                <h2>Opciones</h2>
+                <a href="#">
+                    <img src="../../img/mapas.jpg">
+                    <p>Mapas</p>
+                </a>
+                <a href="#">
+                    <img src="../../img/brawlers.png">
+                    <p>Brawlers</p>
+                </a>
+                <a href="#">
+                    <img src="../../img/armas.png">
+                    <p>Armas</p>
+                </a>
+                <a href="#">
+                    <img src="../../img/rangos.png">
+                    <p>Rango</p>
+                </a>
+            </div>
+    </aside>
+   </div>
+
+</body>
+</html>
+
+
